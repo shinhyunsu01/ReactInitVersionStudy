8a636b269c12d0518393c6af8a1d4f4febccf366
신현수 <sinhyeonsu@MacBook-Pro.local>
2021-12-20 15:57:07 +0900

    init

---
+++ nuber-eats-frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import "./styles/styles.css";
+function App() {
+  return (
+    <div className="bg-black">
+      <h1 className="text-xl text-white">Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
